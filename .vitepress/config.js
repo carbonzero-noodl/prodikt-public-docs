16d3238ac9b34cbc222704adf173b3c196a69663
axelstandar <axel@prodikt.com>
2022-06-16 22:55:53 +0200

    test vitepress

---
+++ .vitepress/config.js
@@ -0,0 +1,10 @@
+const primarySidebar = [{ text: "Our Story", link: "/about/our-story" }];
+
+export default {
+  // These are app level configs.
+  title: 'VitePress',
+  description: 'Vite & Vue powered static site generator.',
+  base: '/base/',
+  lang: 'se-SV',
+  sidebar: primarySidebar
+}
