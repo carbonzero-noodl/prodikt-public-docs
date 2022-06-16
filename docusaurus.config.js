f845f3dfa849cc6afe5a488f00c67cecea0631d4
axelstandar <axel@prodikt.com>
2022-06-16 16:59:03 +0200

    a

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://carbonzero-noodl.github.io/',
-  baseUrl: '/prodikt-public-docs/build',
+  baseUrl: '/prodikt-public-docs',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
